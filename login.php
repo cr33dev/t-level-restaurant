a44abe9f69a76269de8e09d38c3fc6bcb6f0eecb
Luke <thebigbossgirl@outlook.com>
2025-09-16 15:28:13 +0100

    push to team a - new stoof

--- login.php
+++ login.php
@@ -8,6 +8,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
@@ -17,15 +18,16 @@ if (isset($_SESSION['error'])){
 }
 ?>
 
+<div class="container">
+    <form action="verify_login.php" method="post">
+        <label for="#email">Email: </label>
+        <input type="email" required id="email" placeholder="example@email.com">
 
-<form action="verify_login.php" method="post">
-    <label for="#email">Email: </label>
-    <input type="email" required id="email" placeholder="example@email.com">
-
-    <label for="#pass">Password: </label>
-    <input type="password" required id="pass" placeholder=".......">
-    <button type="submit">Log In</button>
-</form>
+        <label for="#pass">Password: </label>
+        <input type="password" required id="pass" placeholder=".......">
+        <button type="submit">Log In</button>
+    </form>
+</div>
 
 <a href="https://bit.ly/nolinkneeded" target="_blank">Forgot Password</a>
 </body>
--- verify_login.php
+++ verify_login.php
@@ -1,1 +1,6 @@
 <?php
+session_start();
+
+$user_email = $_POST['email'];
+$user_email = htmlspecialchars($user_email);
+$user_password = $_POST['pass'];
