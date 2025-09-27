2a283f514763a293f0cacc86da223223c9e160f8
David <132082715+ItzDavidWasTaken@users.noreply.github.com>
2025-09-27 16:23:39 +0100

    Create footer.php
    same thing as nav tbh

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+</main>
+<footer class="website-footer">
+    <div class="footer-container">
+        <p>Phone: Example Number</p>
+        <p>Twitter: @Tlevel</p>
+        <p>Inquiries: Company@tlevel.com</p>
+        <p>Support: Support@tlevel.com</p>
+        <p>Instagram: @Tlevel</p>
+        <p>&copy; T-Level Resturant</p>
+    </div>
+</footer>
+</body>
+</html>
